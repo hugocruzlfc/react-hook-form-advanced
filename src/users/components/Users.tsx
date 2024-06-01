fab280d8a8348da470bc396f7c7eec27d90e96ed
hugocruzlfc <hugocruzlfc@gmail.com>
2024-06-01 14:20:26 +0200

    feat: add main base components

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,13 +1,29 @@
-import { Stack, TextField } from "@mui/material";
+import { Stack, TextField, Typography } from "@mui/material";
 import { UserSchema } from "../types/user.schema";
-import { RHFAutocomplete } from "../../components/RHFAutocomplete";
 import { useFormContext } from "react-hook-form";
+import {
+  useGenders,
+  useLanguages,
+  useSkills,
+  useStates,
+} from "../services/queries";
+import { RHFAutocomplete } from "@/components/RHFAutocomplete";
+import { RHFToggleButtonGroup } from "@/components/RHFToggleButtonGroup";
+import { RHFRadioGroup } from "@/components/RHFRadioGroup";
+import { RHFCheckbox } from "@/components/RHFCheckbox";
+import { RHFDateTimePicker } from "@/components/RHFDateTimePicker";
+import { RHFDateRangePicker } from "@/components/RHFDateRangePicker";
 
 export function Users() {
   const {
     register,
     formState: { errors },
   } = useFormContext<UserSchema>();
+
+  const statesQuery = useStates();
+  const languagesQuery = useLanguages();
+  const gendersQuery = useGenders();
+  const skillsQuery = useSkills();
 
   return (
     <Stack sx={{ gap: 2 }}>
@@ -26,11 +42,28 @@ export function Users() {
       <RHFAutocomplete<UserSchema>
         name="states"
         label="States"
-        options={[
-          { id: "1", label: "Alabama" },
-          { id: "2", label: "Alaska" },
-        ]}
+        options={statesQuery.data}
       />
+      <RHFToggleButtonGroup<UserSchema>
+        name="languagesSpoken"
+        options={languagesQuery.data}
+      />
+      <RHFRadioGroup<UserSchema>
+        name="gender"
+        options={gendersQuery.data}
+        label="Gender"
+      />
+      <RHFCheckbox<UserSchema>
+        name="skills"
+        options={skillsQuery.data}
+        label="Skills"
+      />
+      <RHFDateTimePicker<UserSchema>
+        name="registrationDateAndTime"
+        label="Registration Date & Time"
+      />
+      <Typography>Former Employment Period:</Typography>
+      <RHFDateRangePicker<UserSchema> name="formerEmploymentPeriod" />
     </Stack>
   );
 }
